3e26eb73488456408e17ffdedc2126c5765c900f
Charles2029 <love98982016@gmail.com>
2024-05-21 12:14:33 +0800

    compare the two loop for & for..of

--- dailyTest/main.js
+++ dailyTest/main.js
@@ -23,4 +23,44 @@ console.log(filtered);
         const result =`${i} x ${i} = ${i * i}`;
         console.log(result);
     }
- 
+    const cats = ["Leopard", "Serval", "Jaguar", "Tiger", "Caracal", "Lion"];
+
+    for (const cat of cats) {
+      console.log(`${cat} \n`);
+    }
+    
+    for(let i=0;i<cats.length;i++){
+        console.log(cats[i]);
+    }
+
+    const dogs = ["Pete", "Biggles", "Jasmine"];
+
+let myFavoriteDogs = "My cats are called ";
+
+for (const dog of dogs) {
+  myFavoriteDogs += `${dog}, `;
+}
+
+console.log(myFavoriteDogs); // "My cats are called Pete, Biggles, Jasmine, "
+
+const children=["Isabella","Olivia","Alex"];
+let myBestChildren ="My children are ";
+/*
+for(const child of children){
+    myBestChildren += `${child},`;
+    console.log(myBestChildren);
+
+}
+*/
+
+for (let i=0;i<children.length;i++){
+    if(i===children.length-1){
+        myBestChildren +=`and ${children[i]}`;
+    }else{
+        myBestChildren += `${children[i]},`;
+    }
+
+}
+console.log(myBestChildren);
+
+
