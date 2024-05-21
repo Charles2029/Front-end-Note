c9517a06c26ac35935ae4531079458d2dca8a05f
Charles2029 <love98982016@gmail.com>
2024-05-21 15:08:21 +0800

    add readme.md

---
+++ Skipping iterations with continue/README.md
@@ -0,0 +1,13 @@
+## 
+This is a  note
+###
+this is  the example 
+while  , do ... while loop
+
+
+initializer
+while (condition) {
+  // code to run
+
+  final-expression
+}
--- Skipping iterations with continue/main.js
+++ Skipping iterations with continue/main.js
@@ -11,7 +11,9 @@ btn.addEventListener("click", () => {
     let sqRoot = Math.sqrt(i);// if num=4 . the i will add to 5
     if (Math.floor(sqRoot) !== sqRoot) {
       continue;//continue mean jump over (跳过特定的条件)；
-    }
+    } //If the square root and the rounded down square root do not equal one another (!==), 
+    //it means that the square root is not an integer, so we are not interested in it. In such a case, we use the continue statement to skip on to the next loop iteration
+    // without recording the number anywhere.
     para.textContent += `${i} `;
   }
 });
