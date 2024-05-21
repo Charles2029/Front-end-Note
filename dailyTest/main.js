424c3d9731d42fec0c7b0a469f1f0b6552e24933
Charles2029 <love98982016@gmail.com>
2024-05-21 10:45:06 +0800

    new

---
+++ dailyTest/main.js
@@ -0,0 +1,12 @@
+
+function toUpper(string){
+    return string.toUpperCase();
+
+}
+
+const  family =["isabella","alex","olivia","charles","judy","grandma","im"];
+
+const upperFamily= family.map(toUpper);
+console.log(upperFamily);
+
+
